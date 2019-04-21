ce1c86fe7ea71706ff61bb555cb2218b005449a3
DatPT <phamtiendatkhanh@gmail.com>
2019-04-21 21:15:17 +0700

    CHat socket

--- routes/v1.js
+++ routes/v1.js
@@ -93,7 +93,38 @@ module.exports.setup = function (app) {
     *       422:
     *         description: sai loại tài khoản
     */
+  /**
+    * @swagger
+    * /api/layDanhSachUser:
+    *   get:
+    *     description: get danh sach giao vien hoac phu huynh
+    *     tags: [User]
+    *     produces:
+    *       - application/json
+    *     parameters:
+    *       - name: userID
+    *         description: user id
+    *         in: query
+    *         type: string
+    *         required: true
+    *       - name: typeUser
+    *         description: loai tai khoan dc tra ve api getThongTin 1 = phu huynh  || 2 = giao vien
+    *         in: query
+    *         type: string
+    *         required: true
+    *         enum:
+    *         - "1"
+    *         - "2"
+    *     responses:
+    *       200:
+    *         description: gửi dữ liệu thành công
+    *       401:
+    *         description: token không hợp lệ
+    *       422:
+    *         description: sai loại tài khoản
+    */
   app.post('/api/loginV1', userController.postLoginAsync)
   app.get('/api/getThongTin', userController.getThongTin)
   app.get('/api/layDuLieuHoiThoai', tinNhanAPI.layDuLieuHoiThoai)
+  app.get('/api/layDanhSachUser', userController.layDanhSachUser)
 }
