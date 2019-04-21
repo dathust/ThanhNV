8bf4203f170de63c440ccf9da6654e5024773ef8
DatPT <phamtiendatkhanh@gmail.com>
2019-04-21 21:15:17 +0700

    CHat socket

--- api/tinnhanapi.js
+++ api/tinnhanapi.js
@@ -32,3 +32,23 @@ let layDuLieuHoiThoaiFn = async (idNguoiGui, idNguoiNhan, skip, limit) => {
     }
     return output
 }
+
+exports.layDanhSachHoiThoai =async (req, res) => {
+    let idNguoiGui = req.query.idNguoiGui
+    let skip = req.query.skip
+    let limit = req.query.limit
+     skip = parseInt(skip, 10) || 0
+     limit = parseInt(limit, 10) || 10
+     if (!idNguoiGui || idNguoiGui === undefined) {
+         return res.status(406).send({error: 'Thieu idNguoiGui', data: null})
+     }
+     try {
+        //  let result =await layDuLieuHoiThoaiFn(idNguoiGui, idNguoiNhan, skip, limit)
+         let result =await tinNhanQR.layDanhSachHoiThoaiQR(idNguoiGui, skip, limit)
+         console.log('======>api lay danh sach hoi thoai:' , idNguoiGui);    
+             return res.send({error: null, data: result})
+     } catch (error) {
+         console.log(error);
+         return res.status(500).send({error: error, data: null})
+     }
+}
